c82732d4bc01ef4c4901c39923a82c05df8998c0
Tural Taghiyev <turalrt@code.edu.az>
2021-12-11 23:53:21 +0400

    first commit

---
+++ ConsoleApp21/PowerOf2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    internal class PowerOf2
+    {
+        public int n;
+        public void isPowerOfTwo(int n)
+        {
+            if (n == 0)
+                Console.WriteLine("Enter number again!");
+
+            while (n != 1)
+            {
+                if (n % 2 != 0)
+                    Console.WriteLine("No");
+
+                n = n / 2;
+            }
+            Console.WriteLine("Yes");
+        }
+    }
+}
---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            PowerOf2 powerof2 = new PowerOf2();
+            powerof2.n = 8;
+            powerof2.isPowerOfTwo(powerof2.n);
+        }
+    }
+}
